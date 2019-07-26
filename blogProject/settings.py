87382f9a140cea4bbc941c6f6432d71d4a5d1d9b
tkekdzhd <tkekdzhd@gmail.com>
2019-07-26 22:40:40 +0900

    third

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blogProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['blogProject/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
